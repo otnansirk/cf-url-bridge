1d1269ef34bc2dc5d9e80de02215e4ae84214146
Kris <iam.otnansirk@gmail.com>
2025-07-29 16:21:45 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ export default {
     const url = new URL(request.url);
     const wa_number = url.searchParams.get("n");
     const message = `
-Halo, saya ingin pesan Bright Gas.
-Nama:
-Alamat:
-Produk Pesanan:
-Jam Pemesanan:
-Terima Kasih
-`
+    Halo, saya ingin pesan Bright Gas.
+    Nama:
+    Alamat:
+    Produk Pesanan:
+    Jam Pemesanan:
+    Terima Kasih
+    `
 
+    const encodedMessage = encodeURIComponent(message);
     if (!wa_number) {
       return new Response("Missing 'n' parameter", { status: 400 });
     }
 
-    const target = `https://wa.me/${encodeURIComponent(wa_number)}?text=${message}`;
+    const target = `https://wa.me/${encodeURIComponent(wa_number)}?text=${encodedMessage}`;
 
     return Response.redirect(target, 302);
   }
